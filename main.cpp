e4d647c8f2d8fd70bf9cb68005b7e4f3dc481cb9
Alexandre Hirsch <51078543+alexandrehirschX@users.noreply.github.com>
2021-12-12 21:55:44 +0100

    interactive trial

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <igl/readOFF.h>
 #include <igl/opengl/glfw/Viewer.h>
 #include <igl/unproject.h>
+#include <igl/unproject_onto_mesh.h>
 #include <vector>
 #include <math.h>
 #include <iostream>
@@ -11,6 +12,8 @@
 
 using namespace Eigen;
 
+typedef Triplet<double> T;
+typedef SimplicialCholesky<SparseMatrix<double>> SC;
 
 MatrixXd Vo;
 MatrixXd Vd, Vd2;
@@ -18,31 +21,31 @@ MatrixXd Vd, Vd2;
 MatrixXi Fo;
 
 
+struct State
+{
+  // Rest and transformed control points
+  Eigen::MatrixXd CV, CU;
+  bool placing_handles = true;
+} s;
+
 float calc_cotangent(Vector3d v1, Vector3d v2){
     return 1/tan(acos(v1.normalized().dot(v2.normalized())));
 }
 
 
-bool key_down(igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifier)
-{
-  if (key == '1')
-  {
-    viewer.data().clear();
-    viewer.data().set_mesh(Vo, Fo);
-    viewer.core().align_camera_center(Vo,Fo);
-  }
-  return false;
-}
+
 
 class Shape {
   public:
     MatrixXd V1, V2, W, L, b;
     MatrixXi F;
+    SparseMatrix<double> Z;
     std::vector<std::vector<int>> N;
     std::vector<Matrix3d> R;
     std::vector<int> Fixed;
     std::vector<MatrixXd> V1_diff, w_V1_diff, V2_diff;
     int n_vertices, n_fixed, n_faces;
+    float current_energy;
 
     Shape(MatrixXd Vo, MatrixXi Fo){
       V1 = Vo;
@@ -73,6 +76,7 @@ class Shape {
       Fixed.resize(n_fixed);
       Fixed = Fi;
       get_laplacian_and_b();
+      current_energy = 0;
     }
 
     std::vector<MatrixXd> get_diff(MatrixXd &V){
@@ -107,7 +111,7 @@ class Shape {
       W = MatrixXd::Zero(n_vertices, n_vertices);
       int j;
       float w;
-      for(int i = 0; i < n_faces; i++){
+      for(int i = 0; i < n_vertices; i++){
         for(int n = 0; n < N[i].size(); n++){
           j = N[i][n];
           for(int k = 0; k < N[i].size(); k++){
@@ -134,6 +138,20 @@ class Shape {
           L(n_vertices + I, I) = 1;
         }
 
+        std::vector<T> tripletList;
+        //tripletList.reserve((L > 0).count());
+        for(int i = 0; i < n_vertices + n_fixed; i++){
+          for(int j = 0; j < n_vertices + n_fixed; j++){
+            if (L(i,j) != 0){
+              tripletList.push_back(T(i,j,L(i,j)));
+            }
+          }
+        }
+
+        Z.resize(n_vertices + n_fixed, n_vertices + n_fixed);
+        Z.setFromTriplets(tripletList.begin(), tripletList.end());
+      
+
         b = MatrixXd::Zero(n_vertices + n_fixed, 3);
         int j;
         for(int i = 0; i < n_vertices; i++){
@@ -151,11 +169,13 @@ class Shape {
     void get_rotations(){
       Matrix3d S, U, V;
       int in, j;
+      current_energy = 0;
       for(int i = 0; i < n_vertices; i++){
         S.setZero();
         for(int n = 0; n < N[i].size(); n++){
           j = N[i][n];
           S += w_V1_diff[i].col(j) * V2_diff[i].col(j).transpose();
+          current_energy += W(i,j) * pow((V2_diff[i].col(j) - (R[i] * V1_diff[i].col(j))).norm(),2);
         }
         JacobiSVD<MatrixXd> svd(S, ComputeThinU | ComputeThinV);
         V = svd.matrixV(); 
@@ -178,38 +198,55 @@ class Shape {
           b.row(i) += 0.5 * (R[i] + R[j]) * w_V1_diff[i].col(j);
         }
       }
-      V2 = (L.llt().solve(b)).block(0, 0, n_vertices, 3);
+
+      SC chol(Z);
+      //V2 = (L.inverse()*b).block(0, 0, n_vertices, 3);
+      V2 = (chol.solve(b)).block(0, 0, n_vertices, 3);
     }
 
-
-    float current_energy(){
-      float S;
-      Vector3d X;
-      int j;
-      for(int i = 0; i < n_vertices; i++){
-        for(int n = 0; n < N[i].size(); n++){
-          j = N[i][n];
-          X = V2_diff[i].col(j) - (R[i] * V1_diff[i].col(j)); //(V2.row(i) - V2.row(j)).transpose()
-          //std::cout << X.rows() << " " << X.cols() << std::endl;
-          S += W(i,j) * pow(X.norm(),2);
-        }
-      }
-      return S;
-    }
 
     MatrixXd ARAP(int n_iter){
       //std::cout << current_energy() << std::endl;
+      float old_energy;
       for(int iter = 0; iter < n_iter; iter++){
         V2_diff = get_diff(V2);
-        std::cout << current_energy() << std::endl;
+        old_energy = current_energy;
         get_rotations();
         get_p();
+        std::cout << current_energy << std::endl;
+        if(abs(old_energy - current_energy) < 0.1){
+          std::cout << "Iterations required: " << iter << std::endl;
+          break;
+        }
       }
       return V2;
     }
 
 };
 
+
+// bool key_down(igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifier)
+// {
+//   switch(key){
+//     case '1':
+//     {
+
+//     }
+//     case '2':
+//     {
+
+      
+//     }
+
+//     default:
+//       return false;
+
+//   }
+//   viewer.data().clear();
+//   viewer.data().set_mesh(Vo, Fo);
+//   viewer.core().align_camera_center(Vo,Fo);
+//   return true;
+// }
 
 
 int main(int argc, char *argv[])
@@ -230,20 +267,49 @@ int main(int argc, char *argv[])
     //   Vd2(i,0) -= 50;
     // }
 
-    std::vector<int> Fixed(98);
-    for(int i = 0; i < 98; i++){
-      Fixed[i] = i;
-    }
+    // std::vector<int> Fixed(98);
+    // for(int i = 0; i < 98; i++){
+    //   Fixed[i] = i;
+    // }
 
-    Shape M(Vo, Fo);
-    M.deform(Vd, Fixed);
-    Vd = M.ARAP(10);
+    // Shape M(Vo, Fo);
+    // M.deform(Vd, Fixed);
+    // Vd = M.ARAP(400);
+    
+    
+    igl::opengl::glfw::Viewer viewer;
+
+    viewer.callback_mouse_down = 
+    [&](igl::opengl::glfw::Viewer&, int, int)->bool
+  {
+    RowVector3f last_mouse = Eigen::RowVector3f(
+      viewer.current_mouse_x,viewer.core().viewport(3)-viewer.current_mouse_y,0);
+
+      // Find closest point on mesh to mouse position
+      int fid;
+      Eigen::Vector3f bary;
+      if(igl::unproject_onto_mesh(
+        last_mouse.head(2),
+        viewer.core().view,
+        viewer.core().proj, 
+        viewer.core().viewport, 
+        Vo, Fo, 
+        fid, bary))
+      {
+        long c;
+        bary.maxCoeff(&c);
+        Eigen::RowVector3d new_c = Vo.row(Fo(fid,c));
+        std::cout << Fo(fid,c) << std::endl;
+      }
+
+    return false;
+  };
 
     //M.deform(Vd2, Fixed);
-    //Vd = M.ARAP(5);
+    //Vd = M.ARAP(10);
 
-    igl::opengl::glfw::Viewer viewer;
+    
     //viewer.callback_key_down = &key_down;
-    viewer.data().set_mesh(Vd, Fo);
+    viewer.data().set_mesh(Vo, Fo);
     viewer.launch();
 }
